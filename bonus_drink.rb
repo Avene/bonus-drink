35ae691ca5ea3783191abe261b2f7201ac8513a7
Avene <juc-y@live.jp>
2015-05-20 15:53:31 +0900

    Add bonus calculation

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    raise StandardError if amount < 0
+    empty_bottles = amount
+    drunk = amount
+    while empty_bottles >= 3
+      exchanged = empty_bottles / 3
+      drunk += exchanged
+      empty_bottles -= exchanged * 2
+    end
+    return drunk
   end
 end
